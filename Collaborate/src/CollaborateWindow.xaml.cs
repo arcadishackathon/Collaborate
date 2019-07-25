af60a2b7b204dcdcd1d5a5fc106b34bce32e0270
Laurence Elsdon <git@elsdon.me>
2019-07-25 13:48:32 -0400

    Initial Commit

---
+++ Collaborate/src/CollaborateWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Collaborate
+{
+    /// <summary>
+    /// Interaction logic for CollaborateWindow.xaml
+    /// </summary>
+    public partial class CollaborateWindow : Window
+    {
+        public CollaborateWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
